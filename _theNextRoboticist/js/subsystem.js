137c477db041e48a4b6f09f34bdb9451ec76a657
Drdoomdragon <ayana.beatty@gmail.com>
2018-10-30 23:57:22 -0600

    created menu for subsystem
    experimented with setting up the menu. now have to think about live update for the robot.

--- _theNextRoboticist/js/subsystem.js
+++ _theNextRoboticist/js/subsystem.js
@@ -7,6 +7,8 @@ subsystemState.prototype.preload=function(){
 	newGame.load.atlasJSONHash ('body', 'assets/image/robot_body/bodies/bodies.png', 'assets/image/robot_body/bodies/bodies.json');
 	newGame.load.atlasJSONHash ('wheel', 'assets/image/robot_body/wheels/wheels.png', 'assets/image/robot_body/wheels/wheels.json');
 
+	// invisibleheads.
+	newGame.load.image ('invisiHeadButton', 'assets/image/robot_body/heads/transparent_head.png')
 	newGame.load.image ('confirmButton', 'assets/image/confirm.png');
 }
 
@@ -29,11 +31,16 @@ subsystemState.prototype.create =function(){
 	let mainInstructionsText = newGame.add.text(0,40, 'Instructions:', style);
 
 // MAKING THE ROBLOX CHARACTER
-	chosenHead = newGame.add.button(250,100, 'head', chooseHead);
+	chosenHead = newGame.add.sprite(250,100, 'head', 0);
 	chosenBody = newGame.add.sprite(0,0, 'body',0).alignTo(chosenHead, Phaser.BOTTOM_CENTER, 0, 10);
-	chosenArm_right = newGame.add.sprite(0,0, 'arm', 00).alignTo(chosenBody, Phaser.LEFT_CENTER, -10,0);
-	chosenArm_left = newGame.add.sprite(0,0, 'arm', 0).alignTo(chosenBody, Phaser.RIGHT_CENTER, 10, 0);
+	chosenArm_right = newGame.add.sprite(0,0, 'arm', 5).alignTo(chosenBody, Phaser.LEFT_CENTER, -10,0);
+	chosenArm_left = newGame.add.sprite(0,0, 'arm', 5).alignTo(chosenBody, Phaser.RIGHT_CENTER, 10, 0);
 	chosenWheel = newGame.add.sprite(0, 0, 'wheel', 0).alignTo(chosenBody, Phaser.BOTTOM_CENTER, 0,10);
+
+//creating invisible buttons.
+	let invisiHead = newGame.add.button(250,100, 'invisiHeadButton', chooseHead);
+	invisiHead.alpha =0;
+	let invisiWheel = newGame.add.button();
 
 //confirm button available in case selection is wanted
 	let confirm = newGame.add.button(0,0, 'confirmButton').alignTo(chosenWheel, Phaser.BOTTOM_CENTER, 0, 50);
@@ -50,6 +57,53 @@ function clearScreen_subsystem(){
 }
 
 function chooseHead(){
+	//clears screen
 	newGame.world.removeAll();
+	headDecision = 0;
+
+	//gives instructions
 	let chooseHeadInstructions = newGame.add.text(0,0, 'congrats', style);
+
+	//displays images of the heads types.
+	headOptionA = newGame.add.sprite(0,50, 'head',1);
+	headOptionB = newGame.add.sprite(0,0, 'head', 2).alignTo(headOptionA, Phaser.BOTTOM_CENTER, 0,10);
+	headOptionC = newGame.add.sprite(0,0, 'head', 3).alignTo(headOptionB, Phaser.BOTTOM_CENTER, 0,10);
+
+	//displays descriptions of the head types.
+	/* descriptions to be added later, but they will be adjacent to the headOptions. 
+	    most likely images. */
+
+	// buttons to make head selections.
+	let invisiHeadOptionA = newGame.add.button(0,50, 'invisiHeadButton', chooseHeadA);
+	invisiHeadOptionA.alpha =0;
+
+	let invisiHeadOptionB = newGame.add.button(0,0, 'invisiHeadButton', chooseHeadB).alignTo(invisiHeadOptionA, Phaser.BOTTOM_CENTER, 0,10);
+	invisiHeadOptionB.alpha =0;
+
+	let invisiHeadOptionC = newGame.add.button(0,0, 'invisiHeadButton', chooseHeadC).alignTo(invisiHeadOptionB, Phaser.BOTTOM_CENTER, 0,10);
+	invisiHeadOptionC.alpha =0;
+
+	// functions assigning head options.
+	function chooseHeadA (){
+		headDecision = 1;
+		newGame.world.removeAll();
+		chosenHead = newGame.add.sprite(250,100, 'head',1);
+	}
+
+	function chooseHeadB (){
+		headDecision = 1;
+		newGame.world.removeAll();
+		chosenHead = newGame.add.sprite(250,100, 'head',2);
+	}
+
+	function chooseHeadC (){
+		headDecision = 1;
+		newGame.world.removeAll();
+		chosenHead = newGame.add.sprite(250,100, 'head',3);
+	}
+}
+
+function chooseLeg(){
+	newGame.world.removeAll();
+	let chooseLegsInstructions = newGame.add.text(0,0, 'congrats', style);
 }
